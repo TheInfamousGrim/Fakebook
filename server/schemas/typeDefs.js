031eb6aeac149dfc16fece725f05e70043d9d32f
TheInfamousGrim <finchergeorge1@gmail.com>
2022-12-08 07:41:34 +0000

    Feat: build working server, start creating schema
    Built out a working server.
    
    Used apollo server and tested that everything is working using
    localhost.
    
    Started working on the schema as well.
    
    Next thing to work on are the models.

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,68 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID!
+        createdAt: String!
+        first_name: String!
+        last_name: String!
+        email: String!
+        password: String!
+        gender: String!
+        phoneNumber: String!
+        picture: String
+        cover: String
+        birthYear: Int!
+        birthMonth: Int!
+        birthDay: Int!
+        friends: [User]
+        following: [User]
+        followers: [User]
+        requests: [User]
+        search: [User]
+        userDetails: [UserDetails]
+        savedPosts: [Post]
+    }
+
+    type Auth {
+        token: ID!
+        user: User 
+    }
+
+    type UserDetails {
+        _id: ID!
+        createdAt: String! 
+        bio: String
+        otherName: String
+        job: String
+        workplace: String
+        highSchool: String
+        college: String
+        currentCity: String
+        hometown: String
+        relationShip: String
+        instagram: String 
+    }
+
+    type Post {
+        _id: ID!
+        createdAt: String!
+        type: String
+        text: String
+        images: [String]
+        user: User
+        background: String
+        reacts: [React]
+        comments: [Comment] 
+    }
+
+    type Comment {
+        _id: ID!
+        createdAt: String!
+        text: String
+        image: String
+        commentBy: User
+    }
+
+
+`
--- server/server.js
+++ server/server.js
@@ -1,61 +1,73 @@
 /* ------------------------------ node modules ------------------------------ */
 const express = require('express');
+// Import environment variables
+require('dotenv').config();
 // Use Apollo server
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer, gql } = require('apollo-server-express');
 // Use path to create paths to certain directories
 const path = require('path');
 // Import CORS
 const cors = require('cors');
 
-/* --------------------------- application modules -------------------------- */
-// This is where you import your middleware
-const { authMiddleware } = require('./utils/auth');
-// Get your types from the schema
-const { typeDefs, resolvers } = require('./schemas');
+console.log(process.env.NODE)
+
+/* --------------------------- database and schema -------------------------- */
+// Import typeDefs and resolvers
+// const { typeDefs, resolvers } = require('./schemas');
 // Import the connection to your db
 const db = require('./config/connection');
 
+const typeDefs = gql`
+    type Query {
+        sayHi: String!
+    }
+`
+
+const resolvers = {
+    Query: {
+        sayHi: () => 'Hello big boi'
+    }
+}
+
+/* ------------------------------ server setup ------------------------------ */
 // Get the port you wish to use
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 // Use express as an app
 const app = express();
 // Create a new apollo server
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware,
+        typeDefs,
+        resolvers,
 });
 
 app.use(express.urlencoded({ extended: false }));
 // Make sure express can parse JSON
 app.use(express.json());
 
-// Serve up static assets
-app.use('/images', express.static(path.join(__dirname, '../client/images')));
+/* --------------------------- application modules -------------------------- */
+// This is where you import your middleware
+// const { authMiddleware } = require('./utils/auth');
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-}
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// Configure CORS
+app.use(cors());
+
+// Call the async function to start the server
+// startApolloServer(typeDefs, resolvers);
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
     await server.start();
     server.applyMiddleware({ app });
-
+    
     db.once('open', () => {
         app.listen(PORT, () => {
             console.log(`API server running on port ${PORT}!`);
             console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
         });
+        
     });
 };
-
-// Configure CORS
-app.use(cors());
 
 // Call the async function to start the server
 startApolloServer(typeDefs, resolvers);
