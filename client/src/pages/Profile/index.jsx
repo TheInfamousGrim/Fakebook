c6abcf937c8ddb2fb4919bbc52eeae8955df5dcc
TheInfamousGrim <finchergeorge1@gmail.com>
2022-12-13 20:34:41 +0000

    Bug: photo component now tracked as PhotoGallery

--- client/src/pages/Profile/index.jsx
+++ client/src/pages/Profile/index.jsx
@@ -8,7 +8,7 @@ import Auth from '../../utils/Auth';
 import Nav from '../../components/Nav/index';
 import Cover from '../../components/Cover/index';
 import Intro from '../../components/intro/index';
-import Photos from '../../components/Photos';
+import PhotoGallery from '../../components/PhotoGallery';
 import FriendList from '../../components/Friends';
 import CreatePost from '../../components/createPost';
 import Post from '../../components/Post';
@@ -27,7 +27,7 @@ function Profile() {
             <div className="bg-dark px-52 grid grid-cols-12 mt-4 z-10 gap-4 antialiased">
                 <div className="col-span-5 col-start-1 row-start-1 mt-8 space-y-4">
                     <Intro />
-                    <Photos />
+                    <PhotoGallery />
                     <FriendList />
                 </div>
                 <div className=" flex-row col-span-7 col-start-6 row-start-1 mt-8 space-y-4">
